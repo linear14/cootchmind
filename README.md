ca704e5fd8ae15d469941c599cee2410e42f3af0
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2022-04-01 01:04:21 +0900

    Feat: 그림판 기본 기능 구현

---
+++ README.md
@@ -0,0 +1,9 @@
+## 개발에 참고한 자료들
+
+### 구현 아이디어
+
+- [React를 이용한 그림판 그리기](https://www.youtube.com/watch?v=FLESHMJ-bI0)
+
+### 학습
+
+- [요소의 절대좌표 상대좌표 구하기 (getBoundingClientRect, offsetAPI, pageOffset)](https://mommoo.tistory.com/85)
--- client/src/components/GameBoard/SketchBook.tsx
+++ client/src/components/GameBoard/SketchBook.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useCallback, useEffect, useRef, MouseEvent, useState, useMemo } from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -8,23 +8,99 @@ const Container = styled.div`
 `;
 
 const SketchBook = () => {
+  const [isDrawing, setDrawing] = useState<boolean>(false);
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const contextRef = useRef<CanvasRenderingContext2D | null>(null);
+  const [defaultWidth, defaultHeight] = [useMemo(() => 1080, []), useMemo(() => 720, [])];
+
+  const drawStart = useCallback(({ nativeEvent }: MouseEvent) => {
+    const context = contextRef.current;
+    if (!context) {
+      initCanvasContext();
+      return;
+    }
+
+    const { offsetX, offsetY } = nativeEvent;
+    context.beginPath();
+    context.moveTo(offsetX * 2, offsetY * 2);
+    setDrawing(() => true);
+  }, []);
+
+  const draw = useCallback(
+    ({ nativeEvent }: MouseEvent) => {
+      if (!isDrawing) {
+        return;
+      }
+
+      const context = contextRef.current;
+      if (!context) {
+        initCanvasContext();
+        return;
+      }
+
+      const { offsetX, offsetY } = nativeEvent;
+      context.lineTo(offsetX * 2, offsetY * 2);
+      context.stroke();
+    },
+    [isDrawing]
+  );
+
+  const drawEnd = useCallback(() => {
+    const context = contextRef.current;
+    if (!context) {
+      initCanvasContext();
+      return;
+    }
+
+    context.closePath();
+    setDrawing(() => false);
+  }, []);
+
+  const initCanvas = useCallback((canvas: HTMLCanvasElement | null) => {
+    if (!canvas) {
+      return;
+    }
+    // casvas의 실제 너비를 몇 칸으로 쪼갤건지 (많이 쪼갤수록 세밀한 작업이 가능할 것 같음)
+    canvas.width = defaultWidth * 2;
+    canvas.height = defaultHeight * 2;
+
+    // canvas의 실제 너비 / 높이
+    canvas.style.width = `${defaultWidth}px`;
+    canvas.style.height = `${defaultHeight}px`;
+  }, []);
+
+  const initCanvasContext = useCallback(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) {
+      return;
+    }
+
+    const context = canvas.getContext('2d');
+
+    if (context) {
+      context.lineCap = 'round';
+      context.strokeStyle = 'black';
+      context.lineWidth = 10;
+      contextRef.current = context;
+    }
+  }, []);
 
   useEffect(() => {
     const canvas = canvasRef.current;
     if (canvas) {
-      const ctx = canvas.getContext('2d');
-
-      if (ctx) {
-        ctx.fillStyle = '#000000';
-        ctx.fillRect(0, 0, 1080, 720);
-      }
+      initCanvas(canvas);
+      initCanvasContext();
     }
   }, []);
 
   return (
     <Container>
-      <canvas ref={canvasRef} width={1080} height={720}></canvas>
+      <canvas
+        ref={canvasRef}
+        onMouseDown={drawStart}
+        onMouseMove={draw}
+        onMouseUp={drawEnd}
+      ></canvas>
     </Container>
   );
 };
