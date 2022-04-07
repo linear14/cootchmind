6852547f1c3af8fef18e5f5461177f9fae039f46
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2022-04-07 22:02:51 +0900

    Feat: 방 생성 모달 디자인

--- client/src/components/ui/RoomGeneratorModal.tsx
+++ client/src/components/ui/RoomGeneratorModal.tsx
@@ -8,7 +8,7 @@ const Container = styled.div`
   min-height: 660px;
   max-height: 870px;
   margin: 36px auto 0px;
-  background: black;
+  background-color: rgba(0, 0, 0, 0.4);
 
   display: flex;
   justify-content: center;
@@ -16,18 +16,87 @@ const Container = styled.div`
 `;
 
 const Content = styled.div`
-  width: 300px;
-  height: 200px;
-  background: yellow;
+  width: 400px;
+  height: 280px;
+  background: white;
+  border: 1px solid black;
+  padding: 1rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  h2 {
+    text-align: center;
+    font-size: 1.5rem;
+    line-height: 1.5rem;
+  }
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+
+  input {
+    height: 2rem;
+    line-height: 2rem;
+  }
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  gap: 1rem;
+`;
+
+const GenerateRoomButton = styled.button`
+  flex: 1;
+  height: 36px;
+  line-height: 36px;
+  text-align: center;
+  border: 1px solid black;
+  cursor: pointer;
+
+  &::after {
+    content: '방 만들기';
+  }
+`;
+
+const CloseButton = styled.button`
+  flex: 1;
+  height: 36px;
+  line-height: 36px;
+  text-align: center;
+  border: 1px solid black;
+  cursor: pointer;
+
+  &::after {
+    content: '닫기';
+  }
 `;
 
 interface RoomGeneratorProps {
   onClose: () => void;
 }
+
 const RoomGeneratorModal = ({ onClose }: RoomGeneratorProps) => {
+  const generateRoom = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
   return (
     <Container>
-      <Content onClick={onClose} />
+      <Content>
+        <h2>방 만들기</h2>
+        <Form onSubmit={generateRoom}>
+          <p>방 제목</p>
+          <input placeholder='방 제목을 입력해주세요' />
+        </Form>
+        <Buttons>
+          <GenerateRoomButton onClick={generateRoom} />
+          <CloseButton onClick={onClose} />
+        </Buttons>
+      </Content>
     </Container>
   );
 };
