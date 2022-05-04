22cbf921ce4c17a7942888fa79b4b3eed9f28b6c
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2022-05-05 00:00:03 +0900

    Feat: 대기실 페이지 방 리스트에 방장 / 게임 상태 정보 간단하게 추가

--- client/src/components/RoomList/RoomItem.tsx
+++ client/src/components/RoomList/RoomItem.tsx
@@ -5,6 +5,35 @@ const Container = styled.div`
   width: calc((100% - 16px) / 2);
   height: 120px;
   border: 1px solid black;
+
+  display: flex;
+  flex-direction: column;
+`;
+
+const Title = styled.span`
+  height: 24px;
+  line-height: 24px;
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+const MasterName = styled.span`
+  height: 20px;
+  line-height: 20px;
+  font-size: 14px;
+`;
+
+const GameState = styled.div<{ state: string }>`
+  width: 100px;
+  line-height: 20px;
+  padding-top: 4px;
+  padding-bottom: 4px;
+  font-size: 14px;
+  font-weight: bold;
+  text-align: center;
+
+  border-radius: 10px;
+  background-color: ${({ state }) => (state === 'ready' ? 'green' : 'red')};
 `;
 
 interface RoomItemProps {
@@ -12,8 +41,20 @@ interface RoomItemProps {
   onClickItem: (roomId: string) => void;
 }
 
+const mapGameState = {
+  ready: '대기 중',
+  interval: '게임 중',
+  play: '게임 중'
+};
+
 const RoomItem = ({ item, onClickItem }: RoomItemProps) => {
-  return <Container onClick={() => onClickItem(item.roomId)}>{item.title}</Container>;
+  return (
+    <Container onClick={() => onClickItem(item.roomId)}>
+      <Title>{item.title}</Title>
+      <MasterName>방장 : {item.masterName}</MasterName>
+      <GameState state={item.state}>{mapGameState[item.state]}</GameState>
+    </Container>
+  );
 };
 
 export default RoomItem;
