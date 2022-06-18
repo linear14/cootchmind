0513f2cbc9d25e8453588c8d8c03517e0cb4cad5
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2022-06-19 02:03:08 +0900

    Feat: 색상 선택 ColorPicker로 할 수 있도록 기능 추가

--- client/src/components/GameBoard/Palette/ColorList.tsx
+++ client/src/components/GameBoard/Palette/ColorList.tsx
@@ -1,5 +1,6 @@
-import { useMemo, useRef } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import styled, { css } from 'styled-components';
+import { ChromePicker } from 'react-color';
 
 const Container = styled.div`
   position: relative;
@@ -17,13 +18,20 @@ const Row = styled.div`
   }
 `;
 
-const ColorItem = styled.div<{ colorHex: string }>`
+const ColorItem = styled.div<{ colorHex: string; picker?: boolean }>`
+  position: relative;
   width: calc((100% - 4rem) / 5);
   aspect-ratio: 1;
 
-  border: 1px solid black;
+  border: ${({ picker }) => (picker ? '2px solid black' : '1px solid #aaaaaa')};
   background-color: ${({ colorHex }) => colorHex || 'black'};
   cursor: pointer;
+`;
+
+const ColorPicker = styled(ChromePicker)`
+  position: absolute;
+  top: calc(100% + 4px);
+  right: 0px;
 `;
 
 interface ColorListProps {
@@ -31,21 +39,77 @@ interface ColorListProps {
 }
 
 const ColorList = ({ onClickItem }: ColorListProps) => {
+  const [pickerVisibility, setPickerVisibility] = useState<boolean>(false);
+  const [pickedColor, setPickedColor] = useState<string>('#000000');
   const itemList = useMemo(
     () => [
       ['#000000', '#ff0000', '#ffff00', '#008000', '#0000ff'],
-      ['#800080', '#00ff00', '#964b00', '#ffffff']
+      ['#800080', '#00ff00', '#964b00', '#ffffff', 'picker']
     ],
     []
   );
+
+  const handleColor = useCallback(
+    (hexCode: string) => {
+      setPickedColor(hexCode);
+      onClickItem(hexCode);
+    },
+    [onClickItem]
+  );
+
+  useEffect(() => {
+    const closeModal = (e: Event) => {
+      if ((e.target as Element).closest('.picker')) {
+        return null;
+      }
+      setPickerVisibility(false);
+    };
+
+    if (pickerVisibility) {
+      document.addEventListener('click', closeModal);
+
+      return () => {
+        document.removeEventListener('click', closeModal);
+      };
+    }
+  }, [pickerVisibility]);
 
   return (
     <Container>
       {itemList.map((row, idx) => (
         <Row key={idx}>
-          {row.map((hexCode) => (
-            <ColorItem key={hexCode} colorHex={hexCode} onClick={onClickItem.bind(this, hexCode)} />
-          ))}
+          {row.map((hexCode) => {
+            if (hexCode === 'picker') {
+              return (
+                <ColorItem
+                  key={'picker'}
+                  colorHex={pickedColor}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setPickerVisibility(true);
+                  }}
+                  picker
+                >
+                  {pickerVisibility && (
+                    <ColorPicker
+                      className='picker'
+                      color={pickedColor}
+                      onChange={(e) => handleColor(e.hex)}
+                      disableAlpha={true}
+                    />
+                  )}
+                </ColorItem>
+              );
+            } else {
+              return (
+                <ColorItem
+                  key={hexCode}
+                  colorHex={hexCode}
+                  onClick={handleColor.bind(this, hexCode)}
+                />
+              );
+            }
+          })}
         </Row>
       ))}
     </Container>
